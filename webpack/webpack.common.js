9208edcf40f69e73ab6294d57d5988280b8648fa
Nainik <nainik.m@crestinfosystems.net>
2021-11-11 15:52:20 +0530

    Initial BoilerPlate

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -38,7 +38,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './src/index.html'),
+      template: path.resolve(__dirname, '..', './public/index.html'),
     }),
     // new CopyPlugin({
     //   patterns: [{ from: 'source', to: 'dest' }],
